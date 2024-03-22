e4ec757200660ee49647a5c0731f7802268f2c00
Noura Muhammad <160177373+Nouraarafaa@users.noreply.github.com>
2024-03-22 14:56:51 +0200

    Update Filters.cpp

--- Filters/Filters.cpp
+++ Filters/Filters.cpp
@@ -6,8 +6,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+void Black_and_White(Image& image) {
+    for (int i = 0; i < image.width; ++i) {
+        for (int j = 0; j < image.height; ++j) {
 
-    return 0;
+            unsigned int avg = 0;
+            for (int k = 0; k < image.channels; ++k) {
+                avg += image(i, j, k);
+            }
+            avg = avg / 3;
+            for (int k = 0; k < 3; ++k) {
+                if (avg > 127) {
+                    image(i, j, k) = 255;
+                } else {
+                    image(i, j, k) = 0;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    Image image("mario.jpg");
+    Black_and_White(image);
+    image.saveImage("newMario.jpg");
 }
 
