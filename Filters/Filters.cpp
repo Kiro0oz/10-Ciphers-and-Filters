f26a04fcea0a1bc9ddc0e2170108cee42569ebd0
Noura Muhammad <160177373+Nouraarafaa@users.noreply.github.com>
2024-03-22 19:09:10 +0200

    Update Filters.cpp

--- Filters/Filters.cpp
+++ Filters/Filters.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 void GrayScale() {
     // Read Image
-    cout << "Enter your target image name with extention: ";
+    cout << "Enter the name of the image(with extensions) you want to apply filters on : ";
     string filename;
     cin >> filename;
     Image img(filename);
@@ -23,7 +23,7 @@ void GrayScale() {
             unsigned int avg = 0;
             for (int k = 0; k < img.channels; ++k)
             {
-               avg += img(i, j, k);
+                avg += img(i, j, k);
             }
             avg /= 3;
             for (int l = 0; l < 3; ++l) {
@@ -34,20 +34,20 @@ void GrayScale() {
     }
 
     // Save Image
-    cout << "Please enter image name to store new image\n";
-    cout << "& specify extension .jpg, .bmp, .png, .tga: ";
+    cout << "Please Enter the name of the new filtered image\n";
+    cout << "& specify extension .jpg, .bmp, .png, .tga : ";
     string new_fileName;
     cin >> new_fileName;
     img.saveImage(new_fileName);
 }
 
 void Black_and_White() {
-        // Read Image
-        cout << "Enter your target image name with extention: ";
-        string filename;
-        cin >> filename;
-        Image img(filename);
-    
+    // Read Image
+    cout << "Enter the name of the image(with extensions) you want to apply filters on : ";
+    string filename;
+    cin >> filename;
+    Image img(filename);
+
     // Filter
     for (int i = 0; i < img.width; ++i) {
         for (int j = 0; j < img.height; ++j) {
@@ -68,7 +68,7 @@ void Black_and_White() {
     }
 
     // Save Image
-    cout << "Please enter image name to store new image\n";
+    cout << "Please Enter the name of the new filtered image\n";
     cout << "& specify extension .jpg, .bmp, .png, .tga: ";
     string new_fileName;
     cin >> new_fileName;
@@ -91,7 +91,7 @@ void Filters() {
             Black_and_White();
             break;
         }
-         else {
+        else {
             cout << "Error! Please insert a valid filter!\n";
             continue;
         }
@@ -100,7 +100,7 @@ void Filters() {
 }
 
 int main()
-{   
+{
     // Menu
     cout << "=========================\n";
     cout << "Welcome to Baby Photoshop\n";
