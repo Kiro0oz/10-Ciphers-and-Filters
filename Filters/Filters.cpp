b786d0aa1992563ab04abaef926e340ee70b86dc
Noura Muhammad <160177373+Nouraarafaa@users.noreply.github.com>
2024-03-25 16:35:11 +0200

    Update Filters.cpp

--- Filters/Filters.cpp
+++ Filters/Filters.cpp
@@ -1,3 +1,4 @@
+
 // Main file project
 // Noura => [ 2, 5, 8, 11 ]
 // Eman => [ 3, 6, 9, 12 ]
@@ -68,19 +69,85 @@ void Black_and_White() {
     }
 
     // Save Image
-    cout << "Please Enter the name of the new filtered image\n";
+    cout << "Please Enter the name of the new filtered image \n";
     cout << "& specify extension .jpg, .bmp, .png, .tga: ";
     string new_fileName;
     cin >> new_fileName;
     img.saveImage(new_fileName);
 
 }
+void FlipH() {
+    // Read Image
+    cout << "Enter the name of the image (with extensions) you want to flip horizontally: ";
+    string filename;
+    cin >> filename;
+    Image img(filename);
 
+    for (int i = 0; i < img.width / 2; i++) { 
+        for (int j = 0; j < img.height; j++) {
+            for (int k = 0; k < img.channels; k++) {
+                // Swap pixels horizontally
+                unsigned int temp = img(i, j, k);
+                img(i, j, k) = img(img.width - 1 - i, j, k);
+                img(img.width - 1 - i, j, k) = temp;
+            }
+        }
+    }
+
+    // Save Image
+    cout << "Please Enter the name of the new flipped image \n";
+    cout << "& specify extension .jpg, .bmp, .png, .tga: ";
+    string new_fileName;
+    cin >> new_fileName;
+    img.saveImage(new_fileName);
+}
+
+void FlipV() {
+    // Read Image
+    cout << "Enter the name of the image (with extensions) you want to flip vertically: ";
+    string filename;
+    cin >> filename;
+    Image img(filename);
+
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height / 2; j++) {
+            for (int k = 0; k < img.channels; k++) {
+                // Swap pixels vertically
+                unsigned int temp = img(i, j, k);
+                img(i, j, k) = img(i, img.height - 1 - j, k);
+                img(i, img.height - 1 - j, k) = temp;
+            }
+        }
+    }
+
+    // Save Image
+    cout << "Please Enter the name of the new flipped image \n";
+    cout << "& specify extension .jpg, .bmp, .png, .tga: ";
+    string new_fileName;
+    cin >> new_fileName;
+    img.saveImage(new_fileName);
+}
+void Flip(){
+
+    cout<<"1.Horizontally"<<endl<<"2.Vertically"<<endl;
+    int n;
+    cin>>n;
+    if (n==1){
+        FlipH();
+    }
+    else if (n==2){
+        FlipV();
+    }
+    else{
+        cout<<"Please Enter a valid choice..";
+    }
+}
 // Filters options
 void Filters() {
     char ch;
     cout << "1) Gray Scale Filter\n";
     cout << "2) Black & White Filter\n";
+    cout << "3) Flip image Filter\n";
     cout << "=> ";
     while(true) {
         cin >> ch;
@@ -90,6 +157,9 @@ void Filters() {
         } else if(ch == '2') {
             Black_and_White();
             break;
+        }
+        else if (ch == '3'){
+            Flip();
         }
         else {
             cout << "Error! Please insert a valid filter!\n";
